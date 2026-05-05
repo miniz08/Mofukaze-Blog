cbcd17bf334818c3a88049a6dd03816c05178a55
Administrator <Administrator@example.com>
2026-05-05 17:43:01 +0800

    Somegirl

--- blog_service/prisma/prisma.config.ts
+++ blog_service/prisma/prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@prisma/config'
 export default defineConfig({
   databases: {
     db: {
-      url: "mysql://mofukaze:SuijoYuki514@106.52.220.73:3306/mofukaze"
+      url: process.env.DATABASE_URL || ""
 },
     },
   },
